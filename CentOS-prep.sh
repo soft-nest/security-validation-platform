e0375395207bdbd85d88e146944c6a4747e51e01
Adil Bin Bhutto <binbhuttoadil@gmail.com>
2024-11-23 23:24:53 +0900

    centos requirement installation script modified_v3.0

--- CentOS-prep.sh
+++ CentOS-prep.sh
@@ -21,6 +21,6 @@ sudo docker version
 sudo docker run hello-world || echo "Docker run failed"
 
 # Verbose Curl download for Docker Compose
-curl -v -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo curl -v -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
